d416e3d87062623051e01ca778b39e80478659a0
Dirk Callaway <35826733+dirkcallaway@users.noreply.github.com>
2019-03-27 22:21:37 -0600

    Updated Project page with modals and information

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ $(document).ready(function() {
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       $(".navbar-burger").toggleClass("is-active");
       $(".navbar-menu").toggleClass("is-active");
-
   });
+
+  $(".modal-button").on("click", function () {
+    var modalName = "#" + $(this).attr("data-modal");
+    $(modalName).addClass("is-active");
+  });
+
+  $(".delete, .modal-background").on("click", function () {
+    $(".modal").removeClass("is-active");
+  })
 });
